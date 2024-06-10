a58a91135cdea4a82d1b15642f0634f0c5d7ffdc
hichemtab <konanhichemsinshi@gmail.com>
2024-06-11 00:39:41 +0100

    Update library functions and import methods
    The library's main function has been restructured to include an option method. It checks argument validity, updates settings, and adds any new information to data. The `isDefined` method was imported from `npm-jquery-plugin-helpers/src/helpers`. Additionally, the updateData function was added to update element data.

--- src/lib.js
+++ src/lib.js
@@ -1,11 +1,29 @@
-import helpers from 'npm-jquery-plugin-helpers';
+import {isDefined} from "npm-jquery-plugin-helpers/src/helpers";
 export const LIBRARY_FUNCTION_NAME = function (options = {}, ...args) {
     let results = [];
 
     // noinspection JSUnusedGlobalSymbols
     const methods = {
+        option: function (results, data, args) {
+            if (typeof data.settings === 'undefined') {
+                data.settings = {};
+            }
+            if (!isDefined(args) || args.length < 1 || args.length > 2) {
+                console.error('Arguments number not valid');
+            }
+            else if (args.length === 1) {
+                results.push(data.settings[args[0]]);
+            }
+            else if (args.length === 2) {
+                data.settings[args[0]] = args[1];
+                updateData(data);
+            }
+            return results;
+        },
         destroy: function (results, data) {
-
+            //TODO: remove elements created by the plugin
+            //...
+            $('[data-LIBRARY_FUNCTION_NAME-id="'+data.idSuffix+'"]').data('LIBRARY_FUNCTION_NAME', null);
             return results;
         },
     };
@@ -37,4 +55,9 @@ export const LIBRARY_FUNCTION_NAME = function (options = {}, ...args) {
         }
     });
     return results.length > 1 ? results : (results.length === 0 ? null : results[0]);
+}
+
+const updateData = (data) => {
+    let $element = $('[data-LIBRARY_FUNCTION_NAME-id="'+data.idSuffix+'"]');
+    $element.data('LIBRARY_FUNCTION_NAME', data);
 }
